e54d3f30265e35ba73bb8fbe2b5b1cdec02e5318
Fabio Andrea Petrini <fabioandrea.petrini@noycom.it>
2025-01-22 16:37:22 +0100

    [PN-13608] Add logging functionality to DummyPecService
    Introduced detailed logging in all methods of `DummyPecService` for better traceability and debugging. Added a `logback-base.xml` configuration file for logging setup and updated Lombok settings to use a custom logger.

--- src/main/java/it/pagopa/pn/template/service/DummyPecService.java
+++ src/main/java/it/pagopa/pn/template/service/DummyPecService.java
@@ -7,6 +7,7 @@ import it.pagopa.pn.template.dto.PecInfo;
 import it.pagopa.pn.template.type.PecType;
 import jakarta.mail.Session;
 import jakarta.mail.internet.MimeMessage;
+import lombok.CustomLog;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,90 +19,114 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Getter
 @RequiredArgsConstructor
 @Service
+@CustomLog
 public class DummyPecService implements PnPecService {
     private final ConcurrentHashMap<String, PecInfo> pecMap = new ConcurrentHashMap<>();
     private final DummyPecServiceUtil dummyPecServiceUtil;
 
     @Override
     public Mono<String> sendMail(byte[] message) {
+        log.logStartingProcess("Send mail starting...");
+        AtomicReference<String> originalMessageIdRef = new AtomicReference<>();
+
         return Mono.fromCallable(() -> {
-            // parse message
-            MimeMessage mimeMessage = new MimeMessage(Session.getInstance(new Properties()), new ByteArrayInputStream(message));
+               // parse message
+               MimeMessage mimeMessage = new MimeMessage(Session.getInstance(new Properties()), new ByteArrayInputStream(message));
 
-            // get message info
-            String subject = mimeMessage.getSubject();
-            String from = mimeMessage.getFrom()[0].toString();
-            String replyTo = (mimeMessage.getReplyTo() != null && mimeMessage.getReplyTo().length > 0)
-                    ? mimeMessage.getReplyTo()[0].toString() : null;
-            String receiverAddress = mimeMessage.getAllRecipients()[0].toString();
-            String originalMessageId = mimeMessage.getMessageID();
+               // get message info
+               String subject = mimeMessage.getSubject();
+               String from = mimeMessage.getFrom()[0].toString();
+               String replyTo = (mimeMessage.getReplyTo() != null &&
+                                 mimeMessage.getReplyTo().length > 0) ? mimeMessage.getReplyTo()[0].toString() : null;
+               String receiverAddress = mimeMessage.getAllRecipients()[0].toString();
+               String originalMessageId = mimeMessage.getMessageID();
 
-            // build unique id for acceptance and delivery
-            String acceptanceMessageId = UUID.randomUUID().toString();
-            String deliveryMessageId = UUID.randomUUID().toString();
+               originalMessageIdRef.set(originalMessageId);
 
-            PecInfo acceptanceInfo = PecInfo.builder()
-                                            .messageId(acceptanceMessageId)
-                                            .receiverAddress(receiverAddress)
-                                            .subject(subject)
-                                            .from(from)
-                                            .replyTo(replyTo)
-                                            .pecType(PecType.ACCETTAZIONE)
-                                            .errorMap(Map.of()) // no default error
-                                            .read(false)
-                                            .build();
+               log.info("Received message with subject: {}, from: {}, replyTo: {}, receiverAddress: {}, originalMessageId: {}",
+                        subject, from, replyTo, receiverAddress, originalMessageId);
 
-            PecInfo deliveryInfo = PecInfo.builder()
-                                          .messageId(deliveryMessageId)
-                                          .receiverAddress(receiverAddress)
-                                          .subject(subject)
-                                          .from(from)
-                                          .replyTo(replyTo)
-                                          .pecType(PecType.CONSEGNA)
-                                          .errorMap(Map.of()) // no default error
-                                          .read(false)
-                                          .build();
+               // build unique id for acceptance and delivery
+               String acceptanceMessageId = UUID.randomUUID().toString();
+               String deliveryMessageId = UUID.randomUUID().toString();
 
-            // add message in thread-safe object
-            pecMap.put(acceptanceMessageId, acceptanceInfo);
-            pecMap.put(deliveryMessageId, deliveryInfo);
+               PecInfo acceptanceInfo = PecInfo.builder()
+                                               .messageId(acceptanceMessageId)
+                                               .receiverAddress(receiverAddress)
+                                               .subject(subject)
+                                               .from(from)
+                                               .replyTo(replyTo)
+                                               .pecType(PecType.ACCETTAZIONE)
+                                               .errorMap(Map.of()) // no default error
+                                               .read(false)
+                                               .build();
 
-            return originalMessageId;
-        }).delayElement(java.time.Duration.ofMillis(dummyPecServiceUtil.calculateRandomDelay()));
+               PecInfo deliveryInfo = PecInfo.builder()
+                                             .messageId(deliveryMessageId)
+                                             .receiverAddress(receiverAddress)
+                                             .subject(subject)
+                                             .from(from)
+                                             .replyTo(replyTo)
+                                             .pecType(PecType.CONSEGNA)
+                                             .errorMap(Map.of()) // no default error
+                                             .read(false)
+                                             .build();
+
+               // add message in thread-safe object
+               pecMap.put(acceptanceMessageId, acceptanceInfo);
+               pecMap.put(deliveryMessageId, deliveryInfo);
+
+               return originalMessageId;
+       })
+       .doOnSuccess(messageId -> log.logEndingProcess("Send mail success, message id: " + messageId))
+       .doOnError(throwable -> log.logEndingProcess("Send mail error, message id: " + originalMessageIdRef.get(), false, throwable.getMessage()))
+       .delayElement(java.time.Duration.ofMillis(dummyPecServiceUtil.calculateRandomDelay()));
     }
 
     @Override
     public Mono<PnGetMessagesResponse> getUnreadMessages(int limit) {
+        log.logStartingProcess("Get unread messages starting...");
+        log.info("Limit: {}", limit);
+        StringBuilder unreadMessagesLog = new StringBuilder();
+
         return Mono.fromCallable(() -> {
-            if (limit <= 0) {
-                throw new IllegalArgumentException("Limit must be greater than 0");
-            }
+                       if (limit <= 0) {
+                           throw new IllegalArgumentException("Limit must be greater than 0");
+                       }
 
-            // Filtra i messaggi non letti
-            var unreadMessages = pecMap.values().stream()
-                                       .filter(pecInfo -> !pecInfo.isRead())
-                                       .limit(limit)
-                                       .toList();
+                       // Filtra i messaggi non letti
+                       var unreadMessages = pecMap.values().stream().filter(pecInfo -> !pecInfo.isRead()).limit(limit).toList();
 
-            // Costruisci la lista di byte[] da PecInfo
-            List<byte[]> messageBytes = unreadMessages.stream()
-                                                      .map(dummyPecServiceUtil::convertPecInfoToBytes)
-                                                      .toList();
+                       unreadMessages.stream()
+                                     .map(PecInfo::getMessageId)
+                                     .toList()
+                                     .forEach(messageId -> unreadMessagesLog.append(messageId).append(", "));
 
-            // Crea un oggetto PnListOfMessages
-            PnListOfMessages pnListOfMessages = new PnListOfMessages(messageBytes);
+                       // Costruisci la lista di byte[] da PecInfo
+                       List<byte[]> messageBytes = unreadMessages.stream().map(dummyPecServiceUtil::convertPecInfoToBytes).toList();
 
-            // Restituisci la risposta PnGetMessagesResponse
-            return new PnGetMessagesResponse(pnListOfMessages, unreadMessages.size());
-        }).delayElement(java.time.Duration.ofMillis(dummyPecServiceUtil.calculateRandomDelay()));
+                       // Crea un oggetto PnListOfMessages
+                       PnListOfMessages pnListOfMessages = new PnListOfMessages(messageBytes);
+
+                       // Restituisci la risposta PnGetMessagesResponse
+                       return new PnGetMessagesResponse(pnListOfMessages, unreadMessages.size());
+                   })
+       .doOnSuccess(result -> log.logEndingProcess("Get unread messages success, message ids: " + unreadMessagesLog))
+       .doOnError(throwable -> log.logEndingProcess("Get unread messages error, message ids: " + unreadMessagesLog, false, throwable.getMessage()))
+       .delayElement(java.time.Duration.ofMillis(dummyPecServiceUtil.calculateRandomDelay()));
     }
 
     @Override
     public Mono<Void> markMessageAsRead(String messageID) {
+        log.logStartingProcess("Mark message as read starting...");
+        log.info("Message ID: {}", messageID);
+        AtomicReference<String> originalMessageIdRef = new AtomicReference<>(messageID);
+
         return Mono.fromRunnable(() -> {
             if (messageID == null || messageID.isEmpty()) {
                 throw new IllegalArgumentException("Message ID cannot be null or empty");
@@ -114,18 +139,29 @@ public class DummyPecService implements PnPecService {
             }
 
             message.setRead(true);
-        }).delayElement(java.time.Duration.ofMillis(dummyPecServiceUtil.calculateRandomDelay())).then();
+        })
+       .doOnSuccess(result -> log.logEndingProcess("Mark message as read success with message id: " + originalMessageIdRef.get()))
+       .doOnError(throwable -> log.logEndingProcess("Mark message as read error with message id: " + originalMessageIdRef.get(),
+                                                    false, throwable.getMessage()))
+       .delayElement(java.time.Duration.ofMillis(dummyPecServiceUtil.calculateRandomDelay())).then();
     }
 
 
     @Override
     public Mono<Integer> getMessageCount() {
+        log.logStartingProcess("Get message count starting...");
+
         return Mono.fromSupplier(pecMap::size)
-                   .delayElement(java.time.Duration.ofMillis(dummyPecServiceUtil.calculateRandomDelay()));
+           .doOnSuccess(result -> log.logEndingProcess("Get message count success, value: " + pecMap.size()))
+           .doOnError(throwable -> log.logEndingProcess("Get message count error, value: " + pecMap.size(), false, throwable.getMessage()))
+           .delayElement(java.time.Duration.ofMillis(dummyPecServiceUtil.calculateRandomDelay()));
     }
 
     @Override
     public Mono<Void> deleteMessage(String messageID) {
+        log.logStartingProcess("Delete message starting...");
+        log.info("Message ID: {}", messageID);
+
         return Mono.fromRunnable(() -> {
             if (messageID == null || messageID.isEmpty()) {
                 throw new IllegalArgumentException("Message ID cannot be null or empty");
@@ -136,6 +172,9 @@ public class DummyPecService implements PnPecService {
             if (removedMessage == null) {
                 throw new IllegalArgumentException("Message with ID " + messageID + " not found");
             }
-        }).delayElement(java.time.Duration.ofMillis(dummyPecServiceUtil.calculateRandomDelay())).then();
+        })
+        .doOnSuccess(result -> log.logEndingProcess("Delete message success, message id: " + messageID))
+        .doOnError(throwable -> log.logEndingProcess("Delete message  error, message id: " + messageID, false, throwable.getMessage()))
+        .delayElement(java.time.Duration.ofMillis(dummyPecServiceUtil.calculateRandomDelay())).then();
     }
 }
