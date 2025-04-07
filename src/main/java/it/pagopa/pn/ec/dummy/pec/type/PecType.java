25f88125d5157b53808602c774045ae672d2001e
mottone-dgs <mario.ottone@dgsspa.com>
2025-04-07 15:31:35 +0200

    PN-13608: first release

---
+++ src/main/java/it/pagopa/pn/ec/dummy/pec/type/PecType.java
@@ -0,0 +1,8 @@
+package it.pagopa.pn.ec.dummy.pec.type;
+
+public enum PecType {
+    CONSEGNA,
+    ACCETTAZIONE,
+
+    NON_PEC;
+}
---
+++ src/test/java/it/pagopa/pn/ec/dummy/pec/service/DummyPecServiceMarkMessageAsReadTest.java
@@ -0,0 +1,56 @@
+package it.pagopa.pn.ec.dummy.pec.service;
+
+import it.pagopa.pn.ec.dummy.pec.conf.DummyPecServiceTestConfiguration;
+import it.pagopa.pn.ec.dummy.pec.dto.PecInfo;
+import it.pagopa.pn.library.exceptions.PnSpapiPermanentErrorException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Import(DummyPecServiceTestConfiguration.class)
+@SpringBootTest
+class DummyPecServiceMarkMessageAsReadTest {
+    @Autowired
+    private DummyPecService dummyPecService;
+
+    @BeforeEach
+    void setUp() {
+        dummyPecService.getPecMap().clear();
+    }
+
+    @Test
+    void testMarkMessageAsRead_ShouldMarkMessageAsRead() {
+        String messageID = "test-message-id";
+        dummyPecService.getPecMap().put(messageID, PecInfo.builder()
+                                                          .messageId(messageID)
+                                                          .read(false)
+                                                          .build());
+
+        StepVerifier.create(dummyPecService.markMessageAsRead(messageID))
+                    .verifyComplete();
+
+        assertTrue(dummyPecService.getPecMap().get(messageID).isRead());
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void testMarkMessageAsRead_ShouldThrowExceptionForNullMessageID(String messageID) {
+        StepVerifier.create(dummyPecService.markMessageAsRead(messageID))
+                    .expectErrorMatches(e -> (e instanceof PnSpapiPermanentErrorException && e.getCause() instanceof IllegalArgumentException))
+                    .verify();
+    }
+
+    @Test
+    void testMarkMessageAsRead_ShouldThrowExceptionForNonExistentMessage() {
+        StepVerifier.create(dummyPecService.markMessageAsRead("non-existent-id"))
+                    .expectErrorMatches(e -> (e instanceof PnSpapiPermanentErrorException && e.getCause() instanceof IllegalArgumentException))
+                    .verify();
+    }
+}
